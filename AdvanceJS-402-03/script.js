5cc03c493532cf1e988b22d18afcbcc5ab89c664
Akshaypote8888 <akshaypote08@gmail.com>
2022-12-07 22:15:53 +0530

    done

--- AdvanceJS-402-03/script.js
+++ AdvanceJS-402-03/script.js
@@ -1,20 +1,29 @@
 let inputText = document.getElementById('input_text')
 let addContainer = document.getElementById('add_container')
 
+
 init()
 function init(){
     document.getElementById('add_item').addEventListener('click', addText)
+    var node = document.getElementsByClassName('Delete_button')
+    // console.log(node)
+    for(let i=0;i<node.length;i++){
+            node[i].addEventListener('click',e => {
+                node.remove(e.target)
+            })
+    }
 }
 
 function addText() {
     addContainer.innerHTML += `<div class="list">
-    <li>${inputText.value}</li><button class="Delete_button" onclick = "deleteItem()">Delete</button>
+    <li>${inputText.value}</li><button class="Delete_button" onclick ="deleteButton(event)">Delete</button>
 </div>`
     inputText.value = ''
+    
+    // console.log(node.text)
 }
 
-function deleteItem() {
-    const list = document.getElementById("add_container");
-    list.removeChild(list.firstElementChild);
+function deleteButton(event){
+    event.target.parentElement.remove(event.target)
 }
 
