89618c75f6251fe2e3fd4e189e36c866c655d206
Akshaypote8888 <akshaypote08@gmail.com>
2022-12-07 12:00:20 +0530

    done

--- AdvanceJS-403-01/script.js
+++ AdvanceJS-403-01/script.js
@@ -6,8 +6,8 @@ let buttonName = document.getElementById('button_name')
 
 function myFunction(event) {
     var x = event.key;
-    document.getElementById("button_name").innerHTML = `You pressed <span id="name"></span> `;
-    document.getElementById('name').innerText = x;
+    document.getElementById("button_name").innerHTML = `You pressed<span id="name"></span> `;
+    document.getElementById('name').innerText = "' "+x+" '";
     let unicode= event.which;
     document.getElementById('button_code').innerHTML = `<div id="code"></div>`
     document.getElementById("code").innerText =  unicode;
