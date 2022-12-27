3e7a212d490b4c2f0e44c6d5d03e3dd113549aa9
Akshaypote8888 <akshaypote08@gmail.com>
2022-12-27 16:26:16 +0530

    done

--- LeaderBoard/script.js
+++ LeaderBoard/script.js
@@ -15,6 +15,7 @@ function init() {
 
 
 function addCard() {
+    console.log(playerInfo)
     playerInfo.innerHTML += `<div id="player_card">
     <label id="name" for="name">${firstNameInput.value + " " + lastNameInput.value}
         <br>
@@ -26,6 +27,7 @@ function addCard() {
     <button class="add_5" onclick = "addfive(event)">+5</button>
     <button id="substract_5" onclick = "removefive(event)">-5</button>
 </div>`;
+
 }
 
 function deleteButton(event) {
@@ -50,7 +52,60 @@ function removefive(event) {
     }
 }
 
+var scoreArray = []
+var countryArray = []
+var nameArray = []
+function sortScore() {
+    for (const child of playerInfo) {
+        for (const childs of child) {
+            if (childs.getAttribute('id') === 'score_marks') {
+                scoreArray.push(parseInt(childs.textContent))
+            }
+            if (childs.getAttribute('id') === 'name') {
+                nameArray.push(parseInt(childs.textContent))
+            }
+            if (childs.getAttribute('id') === 'country_name') {
+                countryArray.push(parseInt(childs.textContent))
+            }
+        }
+    }
+
+}
+
 function sort() {
+    sortScore()
+    for (let i = 0; i < scoreArray.length; i++) {
+        for (let j = i + 1; j < scoreArray.length; j++) {
+            if (scoreArray[j] < scoreArray[j + 1]) {
+                let temp = scoreArray[j];
+                scoreArray[j] = scoreArray[j + 1];
+                scoreArray[j + 1] = temp;
+                let temp1 = countryArray[j];
+                countryArray[j] = countryArray[j + 1];
+                countryArray[j + 1] = temp1;
+                let temp2 = nameArray[j];
+                nameArray[j] = nameArray[j + 1];
+                nameArray[j + 1] = temp2;
+            }
+        }
+    }
+    // console.log(scoreArray)
+
+    var index = -1;
+    for (const child of playerInfo) {
+        index++;
+        for (const childs1 of child) {
+            if (childs1.getAttribute('id') === 'score_marks') {
+                childs1.innerText = scoreArray[index]
+            }
+            if (childs1.getAttribute('id') === 'name') {
+                childs1.innerText = nameArray[index]
+            }
+            if (childs1.getAttribute('id') === 'country_name') {
+                childs1.innerText = countryArray[index]
+            }
+        }
+    }
 
 }
 
